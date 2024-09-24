aadd1d8c970a00e2800a4cfb016c8e269d9b8e55
abdulhalim.hafidh <abdulhalim.hafidh@tra.go.tz>
2024-09-24 17:54:32 +0900

    initial commit

---
+++ src/main/java/com/justusdev/springforge/SpringforgeApplication.java
@@ -0,0 +1,13 @@
+package com.justusdev.springforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringforgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringforgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/justusdev/springforge/SpringforgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justusdev.springforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringforgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
